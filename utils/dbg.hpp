a97c62a565c85f4e135eacab7651ad5f5dbd0962
Pavel V <src@yekm.name>
2014-06-13 22:52:15 +0400

    Dbg improvements

--- utils/dbg.hpp
+++ utils/dbg.hpp
@@ -2,8 +2,10 @@
 #define DBG_H
 
 #include <vector>
+#include <set>
 #include <iostream>
 #include <iterator>
+#include <typeinfo>
 
 namespace utils
 {
@@ -32,7 +34,26 @@ struct Dbg
     template <typename T>
     Dbg& operator<< (std::vector<T> v)
     {
-        std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
+        std::cout << " vector<" << typeid(T).name() << ">(";
+        std::copy(v.begin(), v.end(), std::ostream_iterator<T>(std::cout, ", "));
+        std::cout << ")";
+        return *this;
+    }
+
+    Dbg& operator<< (std::vector<char> v)
+    {
+        std::cout << " vector<char>(";
+        std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, ", "));
+        std::cout << ")";
+        return *this;
+    }
+
+    template <typename T>
+    Dbg& operator<< (std::set<T> v)
+    {
+        std::cout << " set<" << typeid(T).name() << ">(";
+        std::copy(v.begin(), v.end(), std::ostream_iterator<T>(std::cout, ", "));
+        std::cout << ")";
         return *this;
     }
 
